256e073dbea8808f793824a1aef1096039b77795
nindoja <nindoja@31a20786-bda6-11de-8e48-abf58d202e37>
2010-02-16 18:36:58 +0000

    Initial DBTest class.  You can use this to test queries to the local database, and eventually to any remote databases.

---
+++ trunk/hci/HCI/DBTest.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using HCI;
+
+namespace HCI
+{
+    public partial class DBTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void executeQuery(object sender, EventArgs e)
+        {
+            Database db = new Database();
+            DataTable dt;
+            Label title = new Label();
+
+            title.Text = "Query Results";
+
+            dt = db.executeQueryLocal(queryString.Text);
+
+            resultsPanel.Controls.Add(title);
+            resultsPanel.Visible = true;
+
+            resultsPanel.Controls.Add(new LiteralControl("<div width=\"100%\" class=\"dbColumns\"><table border=1>"));
+            foreach (DataRow dr in dt.Rows)
+            {
+                resultsPanel.Controls.Add(new LiteralControl("<tr>"));
+                foreach(Object data in dr.ItemArray)
+                {
+                    resultsPanel.Controls.Add(new LiteralControl("<td>" + data.ToString() + "</td>")); 
+                }
+                resultsPanel.Controls.Add(new LiteralControl("</tr>"));
+            }
+            resultsPanel.Controls.Add(new LiteralControl("</table></div>"));
+            
+        }
+    }
+}
