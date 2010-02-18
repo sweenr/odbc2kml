b24a7e1515d8f1a3db1841b84ab837a65dce88c2
nindoja <nindoja@31a20786-bda6-11de-8e48-abf58d202e37>
2010-02-18 06:25:21 +0000

    Fixed the popup box submit button.

--- trunk/hci/HCI/DBTest.aspx.cs
+++ trunk/hci/HCI/DBTest.aspx.cs
@@ -27,21 +27,27 @@ namespace HCI
             DataTable dt;
             Label title = new Label();
 
-            title.Text = "Query Results";
-            title.CssClass = "dbTitleSpan";
-
             dt = db.executeQueryLocal(queryString.Text);
 
-            resultsPanel.Controls.Add(title);
+            
+
             resultsPanel.Visible = true;
 
-            resultsPanel.Controls.Add(new LiteralControl("<div width=\"100%\" class=\"dbColumns\"><table border=1>"));
+            resultsPanel.Controls.Add(new LiteralControl("<span class=\"connectionStyle\">&nbsp;Database Query Results</span>"));
+            resultsPanel.Controls.Add(new LiteralControl("<div class=\"mainBoxP\">"));  
+            //resultsPanel.Controls.Add(new LiteralControl("<div width=\"100%\" class=\"dbColumns\">"));
+            resultsPanel.Controls.Add(new LiteralControl("<table cellpadding=\"5\" cellspacing=\"0\" class=\"mainBox2\">"));
+            
+            resultsPanel.Controls.Add(new LiteralControl("<tr><td>"));
+            resultsPanel.Controls.Add(new LiteralControl("<div class=\"omainBox4\">"));
+            resultsPanel.Controls.Add(new LiteralControl("<table class=\"omainBox5\" cellspacing=\"0\" cellpadding=\"0\">"));
+            
             resultsPanel.Controls.Add(new LiteralControl("<tr>"));
             foreach (DataColumn dc in dt.Columns)
             {
-                resultsPanel.Controls.Add(new LiteralControl("<td><b>" + dc.ColumnName + "</b></td>"));
+                resultsPanel.Controls.Add(new LiteralControl("<td><b>" + dc.ColumnName + "<br/></b></td>"));
             }
-            resultsPanel.Controls.Add(new LiteralControl("</tr>"));
+            resultsPanel.Controls.Add(new LiteralControl("</tr><tr><td><br/></td></tr>"));
 
             foreach (DataRow dr in dt.Rows)
             {
@@ -52,7 +58,13 @@ namespace HCI
                 }
                 resultsPanel.Controls.Add(new LiteralControl("</tr>"));
             }
-            resultsPanel.Controls.Add(new LiteralControl("</table></div>"));
+            resultsPanel.Controls.Add(new LiteralControl("</table>"));
+            resultsPanel.Controls.Add(new LiteralControl("<div align=\"right\" style=\"padding-top: 20px;\">"));
+            resultsPanel.Controls.Add(new LiteralControl("<input type=\"submit\" ID=\"hideResults\" value=\"Hide Results\" class=\"button\" />"));
+            resultsPanel.Controls.Add(new LiteralControl("</div>"));
+            resultsPanel.Controls.Add(new LiteralControl("</td></tr>"));
+            resultsPanel.Controls.Add(new LiteralControl("</div>"));
+            resultsPanel.Controls.Add(new LiteralControl("</span>"));
             
         }
     }
