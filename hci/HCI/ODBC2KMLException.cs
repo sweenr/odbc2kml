bb3d84d0de9e459f10d582fdeef41eba9c7a71c2
nindoja <nindoja@31a20786-bda6-11de-8e48-abf58d202e37>
2010-03-06 20:20:26 +0000

    Added ODBC2KMLException class and updated DBTest to utilize it for example code.

---
+++ hci/HCI/ODBC2KMLException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HCI
+{
+    public class ODBC2KMLException : Exception
+    {
+        public string errorText;
+
+        public ODBC2KMLException(string text)
+        {
+            errorText = text;
+        }
+    }
+}
