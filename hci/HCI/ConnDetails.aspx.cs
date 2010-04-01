63abaa02ee4569e0796f59a8e12877c61628c7fd
mbbowser <mbbowser@31a20786-bda6-11de-8e48-abf58d202e37>
2010-04-01 01:39:37 +0000

    Fixed issue 71.

--- hci/HCI/ConnDetails.aspx.cs
+++ hci/HCI/ConnDetails.aspx.cs
@@ -906,13 +906,16 @@ namespace HCI
 
             if (exists)
             {
-                overColorExists.Visible = true;
+                ErrorHandler eh = new ErrorHandler("Overlay color already exists! Please choose another.", errorPanel1);
+                eh.displayError();
+                //return;
+                //overColorExists.Visible = true;
                 this.AddOverlayPopupExtender.Hide();
                 this.AddOverlayPopupExtender.Show();
             }
             else
             {
-                overColorExists.Visible = false;
+                //overColorExists.Visible = false;
                 ovr.setId(curOverlayCount.ToString());
                 curOverlayCount -= 1;
                 overlayListAvailableToRemove.Add(ovr);
@@ -956,68 +959,6 @@ namespace HCI
             }
             sessionSave();
         }
-
-        /*protected void genDBTCol(object sender, EventArgs e)
-        {
-           
-            Button sendBtn = (Button)sender;
-
-            Label title = new Label();
-
-            //Getting the parameter passed from the button for the table name//
-            title.Text = sendBtn.CommandArgument.ToString();
-            title.CssClass = "dbTitleSpan";
-
-
-            Button latLong = new Button();
-            latLong.ID = "latLong";
-            latLong.Text = "Lat/Long Details";
-            latLong.CssClass = "button";
-            latLong.ToolTip = "Lat/Long Details";
-
-            Button preview = new Button();
-            preview.ID = "preview";
-            preview.Text = "View Table";
-            preview.CssClass = "button";
-            preview.ToolTip = "View Table";
-
-            DBFields0.Visible = false;
-            DBTPanel0.Visible = false;
-
-            DBTPanel.Controls.Add(title);
-            DBTPanel.Visible = true;
-
-            //For column colors, use mod function to determine if even or odd on creation//
-
-            for (int i = 0; i < 4; i++)
-            {
-                Label temp = new Label();
-                if (i % 2 == 0)
-                {
-                    DBFields.Controls.Add(new LiteralControl("<div width=\"100%\" class=\"dbColumns\">"));
-                }
-                else
-                {
-                    DBFields.Controls.Add(new LiteralControl("<div width=\"100%\" class=\"dbColumns2\">"));
-                }
-                
-                //Column Name//
-                temp.Text = "Column " + i.ToString();
-
-                DBFields.Controls.Add(temp);
-                DBFields.Controls.Add(new LiteralControl("</div>"));
-            }
-           
-
-            DBFields.Controls.Add(new LiteralControl("<p></p><div class=\"right\"><table><tr><td>"));
-            DBFields.Controls.Add(latLong);
-            DBFields.Controls.Add(new LiteralControl("</td><td>"));
-            DBFields.Controls.Add(preview);
-            DBFields.Controls.Add(new LiteralControl("</td></tr></table></div>"));
-
-            DBFields.Visible = true;
-
-        }*/
 
         protected void fillIconListFromDatabase()
         {
@@ -2068,7 +2009,7 @@ namespace HCI
 
         protected void closeAddOverlayFunct(object sender, EventArgs e)
         {
-            overColorExists.Visible = false;
+//            overColorExists.Visible = false;
             this.AddOverlayPopupExtender.Hide();
             sessionSave();
         }
