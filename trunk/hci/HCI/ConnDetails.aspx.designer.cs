e1cf73889da39f21de8c5fca7ee83927d527abac
ctank02@hotmail.com <ctank02@hotmail.com@31a20786-bda6-11de-8e48-abf58d202e37>
2010-04-08 19:13:39 +0000

    Preview KML yayyers!

--- trunk/hci/HCI/ConnDetails.aspx.designer.cs
+++ trunk/hci/HCI/ConnDetails.aspx.designer.cs
@@ -1472,13 +1472,13 @@ namespace HCI {
         protected global::System.Web.UI.WebControls.Button saveConn;
         
         /// <summary>
-        /// makeKMLWORK control.
+        /// previewKML control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button makeKMLWORK;
+        protected global::System.Web.UI.WebControls.Button previewKML;
         
         /// <summary>
         /// warningModal control.
